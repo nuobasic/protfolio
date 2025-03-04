0c5fc52f624049a043ed42157d4a029beef62d1d
nuobasic <nuobasic@naver.com>
2025-03-04 21:50:57 +0900

    init setting

---
+++ src/main/kotlin/com/lecture/portfolio/PortfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.lecture.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioApplication>(*args)
+}
